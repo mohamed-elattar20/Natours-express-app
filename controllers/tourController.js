cac349ba7c5634a84bc55a50b5d262e4fb6e0879
Mohamed Elattar <mohamedelattarm819@gmail.com>
2024-06-16 16:20:25 +0300

    feat: added the rest of the monthly-plan database aggregation logic

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -123,7 +123,7 @@ exports.getTourStats = async (req, res) => {
       // { $match: { _id: { $ne: 'EASY' } } },  //We Can repeat the cycle of stages again
     ]);
 
-    res.status(204).json({
+    res.status(200).json({
       status: 'success',
       data: stats,
     });
@@ -136,9 +136,32 @@ exports.getTourStats = async (req, res) => {
 };
 exports.getMonthlyPlan = async (req, res) => {
   try {
-    res.status(204).json({
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      { $unwind: '$startDates' },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      { $addFields: { month: '$_id' } },
+      { $project: { _id: 0 } },
+      { $sort: { numTourStarts: -1 } },
+      // { $limit: 12 },
+    ]);
+    res.status(200).json({
       status: 'success',
-      data: null,
+      data: { plan },
     });
   } catch (error) {
     res.status(404).json({
