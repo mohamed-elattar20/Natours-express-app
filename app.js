204684cab55b3c81c0c58cbdfb7fbd5f72075667
Mohamed Elattar <mohamedelattarm819@gmail.com>
2024-06-22 16:28:47 +0300

    refactor: refactored error handling for the whole app

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorhandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -23,10 +25,16 @@ app.use('/api/v1/users', userRouter);
 
 // Handling unknow routes (Error handling)
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't Find ${req.originalUrl} on this server`,
-  });
+  //
+  // const err = new Error(`Can't Find ${req.originalUrl} on this server`);
+  // err.status = 'failed';
+  // err.statusCode = 404;
+  // next(err)
+
+  next(new AppError(`Can't Find ${req.originalUrl} on this server`, 404));
 });
+
+//  Global Error Handling for app
+app.use(globalErrorhandler);
 
 module.exports = app;
