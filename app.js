dd02033100049ef6de063c6f270d6cce43e77abf
Mohamed Elattar <mohamedelattarm819@gmail.com>
2024-06-22 16:04:19 +0300

    feat: added error handling for unknown routes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/pubilc`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -19,5 +20,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handling unknow routes (Error handling)
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't Find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
