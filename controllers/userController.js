da2f5e9a9f6a0dd35c61aafdf9717df3905608e2
Mohamed Elattar <mohamedelattarm819@gmail.com>
2024-06-29 14:29:01 +0300

    feat: added login users functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,17 @@
-exports.getAllUsers = (req, res) => {
-  try {
-    res.status(200).json({
-      status: 'success',
-      results: 10,
-      data: 'tours',
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: 'failed',
-      message: 'err',
-    });
-  }
-};
+const User = require('../models/userModel');
+// const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  // Send Response
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: { users },
+  });
+});
 exports.createUser = (req, res) => {
   try {
     res.status(200).json({
