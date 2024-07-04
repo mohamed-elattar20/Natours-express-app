eb9c4fb3bcfe88cc25d691961e2e150c9a2c83ec
Mohamed Elattar <mohamedelattarm819@gmail.com>
2024-07-04 15:14:06 +0300

    feat: added update my password functionality

--- models/userModel.js
+++ models/userModel.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minLength: 8,
+    minLength: 5,
     select: false,
   },
   passwordConfirm: {
