d6ec6ca53ca3618ee1cb040af9cb59738abb9a4e
Mohamed Elattar <mohamedelattarm819@gmail.com>
2024-07-05 14:01:21 +0300

    feat: added security features

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
+/* eslint-disable import/order */
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const globalErrorhandler = require('./controllers/errorController');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// Global middlewares
+
+// Set Security Http Headers
+app.use(helmet());
+
+//  Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// this will allow 100 request for each user in time sapn of 1 hour
+const limiter = rateLimit({
+  max: 100, // 100 request per user ( IP )
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many requests from this IP, Please try again in 1 hour!',
+});
+
+app.use('/api', limiter);
+
+//  Body parser , reading data from body into req.body
+app.use(
+  express.json({
+    // will limit the body the user sends to be less than 10kb
+    limit: '10kb',
+  }),
+);
+
+// Serving Static files
 app.use(express.static(`${__dirname}/pubilc`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
