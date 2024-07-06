144ce421fc476a26b2c3b7f351980b402e9e92f1
Mohamed Elattar <mohamedelattarm819@gmail.com>
2024-07-06 17:13:16 +0300

    feat: added reviewController & reviewRoutes with refrences

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -75,6 +76,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // Handling unknow routes (Error handling)
 app.all('*', (req, res, next) => {
