45305e23e9d7c9da2f73e735c4d9b742c03a7c01
Mohamed Elattar <you@example.com>
2025-04-08 18:24:56 +0200

    feat: added nested route for reviews to get all reviews for a specific tourId

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,10 @@ const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  // to get all reviews for a specific tour if there's a tourId in the req url
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
@@ -12,6 +15,10 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  // this is to make sure that the tour id is passed in the body of the request
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id; // from the protect middleware
   const newReview = await Review.create(req.body);
 
   res.status(200).json({
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,7 +3,10 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({
+  // this will allow us to use the tourId param in the review routes as well which is passed in the url in tourRoutes
+  mergeParams: true,
+});
 
 router
   .route('/')
