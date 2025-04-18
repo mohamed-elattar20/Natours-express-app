2616a306c138a66f75de47a39a89eb0dcd934144
mohamed-elattar20 <mohamedelattarm819@gmail.com>
2025-04-18 16:40:17 +0200

    feat: added creating booking after a successfull payment

---
+++ models/bookingModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Booking must belong to a user!'],
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Booking must belong to a tour!'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Booking must have a price!'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    paid: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('user', 'name photo -_id').populate({
+    path: 'tour',
+    select: 'name',
+  });
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
